a83b8911542d9e5b4d871733b5a65ed49c237e91
khaizter <vashh.dev@gmail.com>
2022-08-16 08:14:23 +0800

    Future implementations for cart

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -7,4 +7,13 @@ const router: Router = Router();
 // get cart
 router.get("/", cartControllers.getCart);
 
+// add item to cart
+router.post("/add");
+
+// remove item from cart
+router.delete("/remove");
+
+// update item from cart - for updating quantity of item
+router.put("/:itemId");
+
 export default router;
