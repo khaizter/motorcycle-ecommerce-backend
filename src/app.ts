0c923a034535b2e47e250c8a1154869cc4770a4d
khaizter <vashh.dev@gmail.com>
2022-09-04 22:48:59 +0800

    Adding multer setup

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,11 @@ import express, {
 } from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import multer from "multer";
+import uuid from "uuid";
 const path = require("path");
 import dotenv from "dotenv";
 dotenv.config();
-
 // Routes
 import cartRoutes from "./routes/cart";
 import authRoutes from "./routes/auth";
@@ -18,7 +19,35 @@ import productRoutes from "./routes/product";
 
 const app: Application = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuid.v4() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
