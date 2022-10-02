25038932898cb5e094d3451964c67ae0ec7c2f8c
khaizter <vashh.dev@gmail.com>
2022-10-02 19:30:01 +0800

    product controllers error handling

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -12,59 +12,70 @@ import s3 from "../services/s3-bucket";
 const bucketName = process.env.BUCKET_NAME;
 
 const getProducts = async (req: any, res: Response, next: NextFunction) => {
-  const products = await Product.find();
+  try {
+    const products = await Product.find();
 
-  const mappedProducts = await Promise.all(
-    products.map(async (product) => {
-      const getObjectParams = {
-        Bucket: bucketName,
-        Key: product.imageKey,
-      };
-      const command = new GetObjectCommand(getObjectParams);
-      const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-      return {
-        _id: product._id,
-        name: product.name,
-        description: product.description,
-        price: product.price,
-        imageKey: product.imageKey,
-        imageUrl: url,
-      };
-    })
-  );
+    const mappedProducts = await Promise.all(
+      products.map(async (product) => {
+        const getObjectParams = {
+          Bucket: bucketName,
+          Key: product.imageKey,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+        return {
+          _id: product._id,
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          imageKey: product.imageKey,
+          imageUrl: url,
+        };
+      })
+    );
 
-  res.status(200).json({
-    message: "get products",
-    products: mappedProducts,
-  });
+    res.status(200).json({
+      message: "get products",
+      products: mappedProducts,
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 const postProduct = async (req: any, res: Response, next: NextFunction) => {
-  const { name, description, price } = req.body;
-  console.log(req.user);
-  const image = req.file;
-  const imageUniqueName = v4() + "-" + image.originalname;
-  const params = {
-    Bucket: bucketName,
-    Key: imageUniqueName,
-    Body: image.buffer,
-    ContentType: image.mimetype,
-  };
+  try {
+    const { name, description, price } = req.body;
+    const { type } = req.user.type;
+    if (type !== "admin") {
+      throwError("Not authorized.", 401);
+    }
+    const image = req.file;
+    const imageUniqueName = v4() + "-" + image.originalname;
+    const params = {
+      Bucket: bucketName,
+      Key: imageUniqueName,
+      Body: image.buffer,
+      ContentType: image.mimetype,
+    };
 
-  const command = new PutObjectCommand(params);
+    const command = new PutObjectCommand(params);
 
-  await s3.send(command);
+    await s3.send(command);
 
-  const product = new Product({
-    name,
-    description,
-    price,
-    imageKey: imageUniqueName,
-  });
+    const product = new Product({
+      name,
+      description,
+      price,
+      imageKey: imageUniqueName,
+    });
 
-  const productResult = await product.save();
+    const productResult = await product.save();
 
-  res.status(200).json({ message: "add product", product: productResult });
+    res.status(200).json({ message: "add product", product: productResult });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 export default { getProducts, postProduct };
