827ce7c5e3a3344c927e5213be0d40479ecbb5e5
khaizter <vashh.dev@gmail.com>
2022-11-30 02:03:58 +0800

    Fix order items image url

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 import mongoose from "mongoose";
 
 import Order from "../models/order";
 import throwError from "../utils/throwError";
+
+import s3 from "../services/s3-bucket";
+const bucketName = process.env.BUCKET_NAME;
 
 const getOrderList = async (req: any, res: Response, next: NextFunction) => {
   try {
@@ -12,11 +17,36 @@ const getOrderList = async (req: any, res: Response, next: NextFunction) => {
       throwError("Not authorized.", 401);
     }
 
-    const orders = await Order.find();
+    const ordersDoc = await Order.find();
+
+    const orders = ordersDoc.map((order) => order.toObject());
+    const mappedOrders = await Promise.all(
+      orders.map(async (order) => {
+        const mappedItems = await Promise.all(
+          order.items.map(async (item) => {
+            const getObjectParams = {
+              Bucket: bucketName,
+              Key: item.imageKey as string,
+            };
+            const command = new GetObjectCommand(getObjectParams);
+            const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+            return {
+              productId: item.productId,
+              name: item.name,
+              price: item.price,
+              quantity: item.quantity,
+              imageKey: item.imageKey,
+              imageUrl: url,
+            };
+          })
+        );
+        return await { ...order, items: mappedItems };
+      })
+    );
 
     return res.status(200).json({
       message: "get order list",
-      orders: orders,
+      orders: mappedOrders,
     });
   } catch (err) {
     return next(err);
@@ -28,11 +58,35 @@ const getOrders = async (req: any, res: Response, next: NextFunction) => {
     const userId = req.user._id;
     const userObjectId = new mongoose.Types.ObjectId(userId);
 
-    const orders = await Order.find({ owner: userObjectId });
+    const ordersDoc = await Order.find({ owner: userObjectId });
+    const orders = ordersDoc.map((order) => order.toObject());
+    const mappedOrders = await Promise.all(
+      orders.map(async (order) => {
+        const mappedItems = await Promise.all(
+          order.items.map(async (item) => {
+            const getObjectParams = {
+              Bucket: bucketName,
+              Key: item.imageKey as string,
+            };
+            const command = new GetObjectCommand(getObjectParams);
+            const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+            return {
+              productId: item.productId,
+              name: item.name,
+              price: item.price,
+              quantity: item.quantity,
+              imageKey: item.imageKey,
+              imageUrl: url,
+            };
+          })
+        );
+        return await { ...order, items: mappedItems };
+      })
+    );
 
     return res.status(200).json({
       message: "get order customer",
-      orders: orders,
+      orders: mappedOrders,
     });
   } catch (err) {
     return next(err);
